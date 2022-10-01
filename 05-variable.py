83c078e6940785d7d8fead37722d1617e73dfb1b
satyendra <santyrocker88@gmail.com>
2022-10-02 00:54:49 +0530

    variable

---
+++ 05-variable.py
@@ -0,0 +1,9 @@
+#variable demo
+a="satya" #Here a is consider as variable
+print(a)
+userName="satyendra" #Here userName consider as user in camelcase
+print(userName)
+a,b,c="sam","advika","kumar"#Here multiple variable assigned  in one line and having multiple value
+print(a,b,c)
+x=y=z=1#Here variable x,y,z having same value as 1
+print(x,y,z)
