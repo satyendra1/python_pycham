518e6efd1bd92538c049eb201215d7a13eea3215
satyendra <santyrocker88@gmail.com>
2022-10-02 01:17:24 +0530

    String Concatenation

---
+++ string-concatenation.py
@@ -0,0 +1,14 @@
+#String Concatenation
+firstName="Satyendra"
+lastName="kumar"
+Name=firstName+" "+lastName+" "
+print(Name)
+print("print name as 3 times:",Name*3)
+name="satya"
+age=32
+# id=name+age#Here we can't add string datatype with integer so we need to convert age as string
+id=name+str(age)
+print(id)
+
+
+
