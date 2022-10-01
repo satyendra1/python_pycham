6775272aebf45ada94452466eac8367560a757b4
satyendra <santyrocker88@gmail.com>
2022-10-02 00:02:06 +0530

    escapesequence-execrcise

---
+++ 03-escapesequence-exercisse.py
@@ -0,0 +1,10 @@
+#**********Print below outputs***********
+#This is double \\ backslash
+#This is /\/\/\/\/\ mountain
+#He is        awesome(use secape sequence)
+#\" \n \t \'
+
+print("This is double \\\\")
+print("This is /\\/\\/\\/\\/\\")
+print("He is \t awesome")
+print("\\\" \\n \\t \\\'")
