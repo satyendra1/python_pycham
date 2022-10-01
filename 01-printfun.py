fc119a0770ec06753d94971aa5061632f75c7319
satyendra <santyrocker88@gmail.com>
2022-10-01 18:37:06 +0530

    print_escape

---
+++ 01-printfun.py
@@ -0,0 +1,14 @@
+#Demostrate print function
+
+print("hello")
+
+#escape sequence
+print("line A \nline B")
+print("name\tSatyendra")
+print("this is double \backslash ")
+print("hello \"world\" ")
+print('I\'m satyendra')
+print("backslash \\")
+print("backslash \ ")
+
+
