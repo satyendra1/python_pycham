9691cbfffd5c3e0d8d98dd8b164e6dcd0e15c3c1
satyendra <santyrocker88@gmail.com>
2022-10-01 20:12:07 +0530

    comment

---
+++ 02-comment.py
@@ -0,0 +1,6 @@
+# "#" Symbol is used for single line comment
+#""" triple double quote or single ''' triple quote is used for multiple comment
+print("hello world\nHey\n") #\n used for newline
+""" multiple line comment
+used here """
+print("My name is satya")
