fdfd1fc51990c48fd121aac65afa4d5951cf3b24
satyendra <santyrocker88@gmail.com>
2022-10-03 19:34:32 +0530

    Three numbers Average

---
+++ 09-average-3-numbers.py
@@ -0,0 +1,8 @@
+#Average of three numbers
+num1,num2,num3=input("Enter three numbers:").split()
+print(f"first number is:{num1}")
+print(f"second number is:{num2}")
+print(f"third number is:{num3}")
+print(f"Average of three numbers {int(num1)} {int(num2)} {int(num3)} is:{(int(num1)+int(num2)+int(num3))/3}")
+print("Average of three numbers  is:{}".format(int(num1)+int(num2)+int(num3)/3))
+print(f"Average of three numbers {int(num1)} {int(num2)} {int(num3)} is",int(num1)+int(num2)+int(num3)/3)
