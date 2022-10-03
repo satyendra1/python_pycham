4d5699ec53ac0f79752720ca13023e8717723ab8
satyendra <santyrocker88@gmail.com>
2022-10-03 20:20:26 +0530

    String Indexing

---
+++ 10-string-indexing.py
@@ -0,0 +1,14 @@
+#String Indexing
+#we use square bracket[] for indexing and count started from 0
+#Name="satyendra"
+#s has index 0
+#a has index 1
+#t has index 2
+#..........so on
+Name="Satyendra"
+print(f"print index of 0 is:{Name[0]}")
+print(f"print index of 0 is:{Name[3]}")
+print(f"print index of 0 is:{Name[4]}")
+print(f"print index of 0 is:{Name[-1]}")
+print(f"print index of 0 is:{Name[-2]}")
+print(f"print index of 0 is:{Name[-3]}")
