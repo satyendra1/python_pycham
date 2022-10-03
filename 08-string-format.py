bbd9c91d0ed8cc5333b1c840ab4c2f04fa457cd9
satyendra <santyrocker88@gmail.com>
2022-10-03 12:12:41 +0530

    string format

---
+++ 08-string-format.py
@@ -0,0 +1,7 @@
+#string Format
+name="satya"
+age=32
+print("My name is %s and age is %d"%(name,age))#python 2 syntax
+print("My name is {} and age is {}".format(name,age)  )#python 3 syntax
+print(f"My name is {name} and age is {age}")#python 3.6 syntax
+print("my name is",name,"and age is",age)#another syntax
